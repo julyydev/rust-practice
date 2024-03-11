22f616fba5d3936d4e14a65050bdaf3d878c04e6
곽재영 <kjyjykwak@gmail.com>
2024-03-11 23:01:28 +0900

    [Feat] practice ch04-02-reference-and-borrow

---
+++ ch04-02-reference-and-borrow/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut s = String::from("hello");
+
+    let r1 = &s; // 문제없음
+    let r2 = &s; // 문제없음
+    println!("{} and {}", r1, r2);
+    // 이 지점 이후로 변수 r1과 r2는 사용되지 않습니다
+
+    let r3 = &mut s; // 문제없음
+    println!("{}", r3);
+
+}
