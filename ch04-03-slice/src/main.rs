35dd3c3f03e68030e2bbea559b47c1b187e55553
곽재영 <kjyjykwak@gmail.com>
2024-03-11 23:44:13 +0900

    [Feat] practice ch04-03-slice

---
+++ ch04-03-slice/src/main.rs
@@ -0,0 +1,36 @@
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i];
+        }
+    }
+
+    &s[..]
+}
+
+fn main() {
+    let my_string = String::from("hello world");
+
+    // `first_word`는 `String`의 일부 혹은 전체 슬라이스에 대해 작동합니다
+    let word = first_word(&my_string[0..6]);
+    let word = first_word(&my_string[..]);
+    // 또한 `first_word`는 `String`의 전체 슬라이스와 동일한 `String`의
+    // 참조자에 대해서도 작동합니다
+    let word = first_word(&my_string);
+
+    let my_string_literal = "hello world";
+
+    // `first_word`는 문자열 리터럴의 일부 혹은 전체 슬라이스에 대해 작동합니다
+    let word = first_word(&my_string_literal[0..6]);
+    let word = first_word(&my_string_literal[..]);
+
+    // 문자열 리터럴은 *곧* 문자열 슬라이스이므로,
+    // 아래의 코드도 슬라이스 문법 없이 작동합니다!
+    let word = first_word(my_string_literal);
+
+    let a = [1, 2, 3, 4, 5];
+
+    let slice = &a[1..3];
+}
